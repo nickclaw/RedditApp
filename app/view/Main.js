b2f651ac067bee0571619f025953fac22ef9a9b8
Nicholas Clawson <nickclaw@gmail.com>
2013-02-28 00:48:20 -0800

    Post toolbar and basic dataview setup
    Added a toolbar for choosing subreddit, sorting, and time.
    Added basic setup for dataview.

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,7 +1,7 @@
 Ext.define('RedditApp.view.Main', {
 	extend: 'Ext.Container',
 	xtype: 'main',
-	requires: [],
+	requires: ['Ext.Button', 'Ext.Toolbar', 'RedditApp.plugin.DataViewPaging', 'Ext.SegmentedButton', 'Ext.DataView'],
 
 	config: {
 		fullscreen: true,
@@ -9,25 +9,90 @@ Ext.define('RedditApp.view.Main', {
 		id: 'main',
 
 		items: [
-			{
+			{ // top toolbar with logo
 				xtype: 'toolbar',
 				docked: 'top',
-				title: {
+				title: { // title in center of titlebar
 					xtype: 'container',
 					centered: true,
 					layout: 'hbox',
 					items: [
-						{
+						{ // reddit logo image
 							xtype: 'component',
 							id: 'logo'
 						},
-						{
+						{ // subreddit text space
 							xtype: 'component',
 							html: '/',
 							id: 'logoText'
 						}
 					]
-				}
+				},
+
+				defaults: {
+					xtype: 'button',
+					iconMask: 'true',
+					ui: 'action',
+				},
+				items: [
+					{ // refresh page button
+						iconCls: 'refresh',
+						id: 'refresh',
+					},
+					{xtype: 'spacer'},
+					{ // settings button
+						iconCls: 'settings',
+						id: 'settings'
+					}
+				]
+			},
+			{ // toolbar for subreddit, sorting, and time
+				xtype: 'toolbar',
+				docked: 'top',
+
+				items: [
+					{xtype: 'spacer'},
+					{ // button to press to open list of subreddits
+						xtype: 'button',
+						id: 'subredditButton',
+						text: 'frontpage'
+					},
+					{ // segmented buttons to choose sorting
+						xtype: 'segmentedbutton',
+						id: 'sortingButtons',
+						allowMultiple: false,
+
+						items: [
+							{
+								text: 'hot',
+								pressed: true,
+							},
+							{text: 'new'},
+							{text: 'rising'},
+							{text: 'controversial'},
+							{text: 'top'}
+						]
+					},
+					{ // button to press to choose time
+						xtype: 'button',
+						id: 'timeButton',
+						iconCls: 'time',
+						iconMask: true,
+						disabled: true
+					},
+					{xtype: 'spacer'}
+				]
+			},
+			{
+				xtype: 'dataview',
+				plugins: [
+					{
+						xclass: 'RedditApp.plugin.DataViewPaging'
+					}
+				],
+				store: 'PostStore',
+				id: 'posts',
+				itemTpl: '<div>{title}</div>'
 			}
 		]
 	}
