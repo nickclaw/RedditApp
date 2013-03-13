10f7debd15bcb4ea4b1a525a5127c6df722d702d
Nicholas Clawson <nickclaw@gmail.com>
2013-03-13 14:18:19 -0700

    Better subreddit loading
    Loads top 300 subreddits automatically instead of having to keep a file
    in the application.

--- app/store/AllSubredditStore.js
+++ app/store/AllSubredditStore.js
@@ -1,13 +1,17 @@
 Ext.define('RedditApp.store.AllSubredditStore', {
 	extend: 'Ext.data.Store',
+	requires: ['Ext.data.proxy.JsonP'],
 
 	config: {
 		model: 'RedditApp.model.Subreddit',
-		autoLoad: true,
+		autoLoad: {addRecords: true},
+		pageSize: 100,
 
 		proxy: {
-			type: 'ajax',
-			url: 'resources/startup/allsubreddits.json',
+			type: 'jsonp',
+			url: 'http://www.reddit.com/reddits/popular/.json',
+			callbackKey: 'jsonp',
+			pageParam: false,
 
 			reader: {
 				type: 'json',
@@ -19,19 +23,41 @@ Ext.define('RedditApp.store.AllSubredditStore', {
 		sorters: [
 			{
 				sorterFn: function(a,b) {
-						if (b.data.title === 'redditNICK'){
-							return 1;
-						}
-						if (a.data.title === 'redditNICK'){
-							return -1;
-						}
-						
-						var aName = a.data.url.toLowerCase();
-						var bName = b.data.url.toLowerCase();
-						return aName > bName ? 1: (aName == bName ? 0 : -1);
-					},
+					if (b.data.title === 'redditNICK'){
+						return 1;
+					}
+					if (a.data.title === 'redditNICK'){
+						return -1;
+					}
+					
+					var aName = a.data.url.toLowerCase();
+					var bName = b.data.url.toLowerCase();
+					return aName > bName ? 1: (aName == bName ? 0 : -1);
+				},
 				order: 'ASC'
 			}
-		]
+		],
+
+		listeners: {
+			beforeload: function() {
+				if (this.getAllCount() == 0){
+					this.add([
+							{
+								title: 'redditNICK',
+								url: undefined
+							}
+						]);
+				}
+			},
+
+			load: function(me, records) {
+				console.log(records);
+				if (me.getAllCount() < 300) {
+					this.getProxy().setUrl('http://www.reddit.com/reddits/popular/.json?after=' + records[records.length-1].data.name);
+					console.log("loading more..." + this.getProxy().getUrl());
+					this.load({addRecords: true});
+				}
+			}
+		}
 	}
 });
